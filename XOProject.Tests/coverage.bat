bd74b2326f2f751b557e60ba0a5a4a6fe152c269
Owoeye Oluwasola Oluwafemi <famofash@gmail.com>
2018-10-04 10:46:44 -0400

    initial commit

---
+++ XOProject.Tests/coverage.bat
@@ -0,0 +1,15 @@
+@echo off
+
+dotnet clean
+dotnet build /p:DebugType=Full
+dotnet minicover instrument --workdir ../ --assemblies XOProject.Tests/**/bin/**/*.dll --sources XOProject/**/*.cs --exclude-sources XOProject/Migrations/**/*.cs --exclude-sources XOProject/*.cs --exclude-sources XOProject\Repository\ExchangeContext.cs
+
+dotnet minicover reset --workdir ../
+
+dotnet test --no-build
+dotnet minicover uninstrument --workdir ../
+dotnet minicover report --workdir ../ --threshold 60
+
+dotnet minicover htmlreport --workdir ../ --threshold 60
+
+
---
+++ XOProject/Repository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XOProject
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> GetAsync(string id);
+
+        IQueryable<T> Query();
+       
+        Task InsertAsync(T entity);
+
+        Task UpdateAsync(T entity);
+    }
+}
